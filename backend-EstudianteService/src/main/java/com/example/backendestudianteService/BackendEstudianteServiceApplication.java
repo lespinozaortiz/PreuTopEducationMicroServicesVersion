20f49ec797b0efcaf9f7e76bb2ee2d1d6ba03c09
Leonardo Espinoza <leonardo.espinoza.o@usach.cl>
2023-11-04 19:45:58 -0300

    creación backend y MS

---
+++ backend-EstudianteService/src/main/java/com/example/backendestudianteService/BackendEstudianteServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendestudianteService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendEstudianteServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendEstudianteServiceApplication.class, args);
+	}
+
+}
---
+++ backend-EurekaService/src/test/java/com/example/backendestudianteService/BackendEstudianteServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendestudianteService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendEstudianteServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
