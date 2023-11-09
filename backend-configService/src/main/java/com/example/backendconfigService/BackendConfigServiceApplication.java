196a9a7a4048bbe9b11e57769e9b81a2958f55fc
Leonardo Espinoza <leonardo.espinoza.o@usach.cl>
2023-11-09 20:39:07 -0300

    cambios backend

--- backend-configService/src/main/java/com/example/backendconfigService/BackendConfigServiceApplication.java
+++ backend-configService/src/main/java/com/example/backendconfigService/BackendConfigServiceApplication.java
@@ -2,8 +2,9 @@ package com.example.backendconfigService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.config.server.EnableConfigServer;
 @SpringBootApplication
+@EnableConfigServer
 public class BackendConfigServiceApplication {
 
 	public static void main(String[] args) {
