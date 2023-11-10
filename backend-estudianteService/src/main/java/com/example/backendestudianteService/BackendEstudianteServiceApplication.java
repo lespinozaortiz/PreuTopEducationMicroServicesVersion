2e9ff14fbca959e1ded8df2f5f7771948eb373a5
Leonardo Espinoza <leonardo.espinoza.o@usach.cl>
2023-11-10 02:14:13 -0300

    cambios implementación servicios

--- backend-estudianteService/src/main/java/com/example/backendestudianteService/BackendEstudianteServiceApplication.java
+++ backend-estudianteService/src/main/java/com/example/backendestudianteService/BackendEstudianteServiceApplication.java
@@ -2,8 +2,11 @@ package com.example.backendestudianteService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
+
 public class BackendEstudianteServiceApplication {
 
 	public static void main(String[] args) {
