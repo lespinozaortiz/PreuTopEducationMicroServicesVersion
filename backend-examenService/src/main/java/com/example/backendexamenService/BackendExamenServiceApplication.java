f9ebc8005f5343ad9a3fb65ba9330467caf5f183
Leonardo Espinoza <leonardo.espinoza.o@usach.cl>
2023-11-11 00:27:14 -0300

    update EurekaService

--- backend-examenService/src/main/java/com/example/backendexamenService/BackendExamenServiceApplication.java
+++ backend-examenService/src/main/java/com/example/backendexamenService/BackendExamenServiceApplication.java
@@ -2,8 +2,10 @@ package com.example.backendexamenService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class BackendExamenServiceApplication {
 
 	public static void main(String[] args) {
